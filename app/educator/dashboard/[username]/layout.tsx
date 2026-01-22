e4e1575cea54a4e4aa4e6f94470bdfa931b19087
mayanksharmams0000-png <mayanksharmams0000@gmail.com>
2026-01-22 20:53:10 +0530

    sec

--- app/educator/dashboard/[username]/layout.tsx
+++ app/educator/dashboard/[username]/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import type { ReactNode } from "react"
-import DynamicBubbles from "@/components/dynamic-bubbles"
+
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
